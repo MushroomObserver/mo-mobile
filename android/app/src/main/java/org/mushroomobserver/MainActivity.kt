9148e9a4368750d946d6d49d5533cb85fd1721d2
Nathan Wilson <nathan@collectivesource.com>
2024-03-07 21:30:40 -0500

    Switch from com.mushroomobserver or the more accurate org.mushroomobserver

--- android/app/src/main/java/org/mushroomobserver/MainActivity.kt
+++ android/app/src/main/java/org/mushroomobserver/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mushroomobserver
+package org.mushroomobserver
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/org/mushroomobserver/MainApplication.kt
+++ android/app/src/main/java/org/mushroomobserver/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.mushroomobserver
+package org.mushroomobserver
 
 import android.app.Application
 import com.facebook.react.PackageList
