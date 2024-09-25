c546f35538b575d3ce34af0807b10b138be04f19
Nathan Wilson <nathan@collectivesource.com>
2024-09-26 00:22:59 +0100

    More Field Slip Code support

--- app/screens/ViewObservation.tsx
+++ app/screens/ViewObservation.tsx
@@ -117,7 +117,7 @@ const ViewObservation = ({
                   </Text>
                   <Text text90R grey10>
                     {observation?.latitude} {observation?.longitude}{' '}
-                    {observation?.altitude}m
+                    {observation?.altitude ? `#${observation?.altitude}m` : ''}
                   </Text>
                   <View row centerV>
                     <Icon
@@ -140,6 +140,12 @@ const ViewObservation = ({
                   </Text>
                 </View>
               </View>
+              <Text text90M grey10>
+                Field Slip Code
+              </Text>
+              <Text text90R grey10>
+                {observation.code?}
+              </Text>
               <Text text90M grey10>
                 Notes
               </Text>
